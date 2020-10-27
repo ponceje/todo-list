00ab6e4b841ae9ca1112524c507c5db47475596a
redadanza <ADANZRA@OOCL.COM>
2020-10-27 22:34:33 +0800

    [react-todo-list] [Jeyem and Raymond] Design: added some design

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4} from 'uuid';
+import swordSound from '../sword-sound.mp3';
+import heyhey from '../heyhey.mp3';
 
 class TodoGenerator extends Component {
     
 onSubmit = (event) => {
+    const beep = new Audio(swordSound);
+    const heya = new Audio(heyhey);
+    beep.play();
     event.preventDefault();
     const text = event.target.todoText.value;
     const id = uuidv4();
     const todo ={ id:id, text: text, done: false};
+    if (text === "cloud"){
+        heya.play();
+    }
 
     this.props.toAddTodo(todo);
 }
     render() {
+        
+
         return (
             <div id="toDoGenerator">
                 <form onSubmit={this.onSubmit}>
