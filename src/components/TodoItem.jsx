18f59a64820b1fac7c4ce27bdbddd890b0fd83f5
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-29 18:05:25 +0800

    [TodoItem API][ Jeyem Raymond] Fix: update delete validation

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,18 +4,29 @@ import { updateTodos } from '../apis/todos';
 import happy from '../happy.mp3';
 
 class TodoItem extends Component {
+   
+constructor(props) {
+    super(props);
+    this.state={
+        delete: false
+    }
+}
+
 
 markList = (event) => {
     const doThat = new Audio(happy);
     doThat.play();
     event.target.classList.toggle('checked');
+    if(this.state.delete){
     updateTodos(this.props.todo).then(response=>{
         this.props.updateTodos(response.data.id);
     })
+}
 
 
 }
 removeItem = (todoid) => {
+    this.state.delete = true;
     removeFromList(todoid).then(response=>{
         this.props.removeFromList(todoid);
     })
@@ -24,13 +35,8 @@ removeItem = (todoid) => {
     render() {
         return (
             <div id={this.props.todo.id}> 
-<<<<<<< HEAD
-                <li onClick={this.markList}  class={this.props.todo.done == true ? "checked" : "unchecked"}>
-                 {this.props.todo.todoText}
-=======
                 <li onClick={this.markList}  class={this.props.todo.done === true ? "checked" : "unchecked"}>
                  {this.props.todo.text}
->>>>>>> c839ca4921fe3325859e30ac6a3f89c779de6f29
                     <span class="close" onClick={()=> this.removeItem(this.props.todo.id)}>&times;</span>
                 </li>
             </div>
